dabe8f9d803e0c095d79c9ade9aa717ecdd6fc13
francisco-codefresh <francisco@codefresh.io>
2018-12-26 12:07:44 -0600

    Creation of Dockerfile and script

---
+++ script/updategitsubmodules.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo $(pwd)
+echo $(ls)
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "ERROR: \$GITHUB_TOKEN var is not set or empty"
+    exit
+fi
+
+echo "Updating git submodules"
+
+mkdir ~/.ssh
+ssh-keyscan github.com > ~/.ssh/known_hosts
+sed -i 's/git@/https:\/\//' .gitmodules
+sed -i 's/ssh:\/\///' .gitmodules
+sed -i 's/github.com:/github.com\//' .gitmodules
+sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_TOKEN@github.com/" .gitmodules
+
+if [ "$CF_SUBMODULE_SYNC" = "true" ]; then
+  echo "\$CF_SUBMODULE_SYNC var is set to 'true'. Syncing submodules"
+  git submodule sync
+fi
+
+git submodule update --init
+echo "Git submodules were updated"
